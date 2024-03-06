1829ea95858a10fd08584f598faa0310d654d2f6
swathipkraja <157653476+swathipkraja@users.noreply.github.com>
2024-03-06 22:07:13 +0000

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,6 @@
 user_msg = input("Enter the message to be printed:")
-print(user_msg)
+no_of_times = int(input("Enter the number of times the message should be printed:"))
+
+
+for i in range(no_of_times):
+    print(user_msg)
